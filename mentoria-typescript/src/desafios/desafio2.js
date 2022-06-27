2cbbd8085d3610b1268abff005dff7065c16636c
azurefirmo <azure_firmo@protonmail.com>
2022-06-27 16:49:59 -0300

    Introdução Prática ao TypeScript

---
+++ mentoria-typescript/src/desafios/desafio2.js
@@ -0,0 +1,23 @@
+// Como podemos melhorar esse código utilizando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "Maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "Atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "Roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "Laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "Carlos",
+    idade = 19,
+    profissao = "Padeiro"
+}
